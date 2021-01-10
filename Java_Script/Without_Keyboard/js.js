aed0293e831bb344964fd7b49f5465a98d50cee2
Enrico <56918333+EnryBarto@users.noreply.github.com>
2021-01-10 21:51:26 +0100

    Tolti commenti

--- Java_Script/Without_Keyboard/js.js
+++ Java_Script/Without_Keyboard/js.js
@@ -4,47 +4,47 @@ function Calculator(){
 		window.alert("ISTRUZIONI PER L'USO DELLA CALCOLATRICE:\n\n• Utilizzare il punto in sostituzione alla virgola (10,5 → 10.5);\n\n• Quando richiesto, inserire il primo numero;\n\n• Quando richiesto, inserire l'operatore:\n\n+ → Addizione\n\n- → Sottrazione\n\n* → Moltiplicazione\n\n/ → Divisione\n\n^ → Potenza;\n\nradQ → Radice quadrata;\n\n• Quando richiesto, inserire il secondo numero;\n\nN.B.: In caso di potenza il primo numero rappresenta la base e il secondo numero rappresenta l'esponente.");
 	}
 
-	sessionStorage.setItem("Already opened?", "Yes"); //salva un cookie con yes
+	sessionStorage.setItem("Already opened?", "Yes");
 
 	var x = prompt("Inserire il primo numero")*1;
 	var op = prompt("Inserire l'operatore");
 	
-	if ( op == "+") { //somma
+	if ( op == "+") {
 		var y = prompt("Inserire il secondo numero")*1;
 	    var result = x + y;
 	    window.alert("Il risultato è: "+result);
 	}
-	else if ( op == "-") { //sottrazione 
+	else if ( op == "-") { 
 		var y = prompt("Inserire il secondo numero")*1;
 	    var result = x - y;
 	    window.alert("Il risultato è: "+result);
 	}
-	else if ( op == "*") { //moltiplicazione
+	else if ( op == "*") {
 		var y = prompt("Inserire il secondo numero")*1;
 	    var result = x * y;
 	    window.alert("Il risultato è: "+result);
 	}
-	else if ( op == "^") { //eleva a potenza 
+	else if ( op == "^") { 
 		var y = prompt("Inserire il secondo numero")*1;
 		var result = 1*1;
-	    for (step = 0; step < y; step++) { //non so perche cetti usa i cicli
+	    for (step = 0; step < y; step++) {
 	    	var result = result * x;
 	    }
 	    window.alert("Il risultato è: "+result);
 	}
-	else if ( op == "sqrt") { //radice quadrata
-		if ( x >= 0) { //verifica se esiste la radice
-			var result = Math.sqrt(x);  //effettua la radice quad
+	else if ( op == "sqrt") {
+		if ( x >= 0) {
+			var result = Math.sqrt(x);
 	    	window.alert("Il risultato è: "+result);
 		}
-		else if (x < 0) { //verifica se esiste
+		else if (x < 0) {
 			window.alert("La radice quadrata di un numero negativo non esiste");
 		}
 		else {
 			window.alert("Il risultato è: NaN");
 		}
 	}
-	else if ( op == "/") { //divisione
+	else if ( op == "/") {
 		if ( y != 0) {
 	        var result = x / y;
 	        window.alert("Il risultato è: "+result);
@@ -63,5 +63,5 @@ function Calculator(){
 		window.alert("Operatore non riconosciuto");
 	}
 
-	window.location.reload(true); //reloadda la calcolatrice 
+	window.location.reload(true);
 }
